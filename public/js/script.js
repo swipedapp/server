1563d91e0688aa4833aa6d37f1e690c39529f577
Adam Demasi <adam@hbang.com.au>
2025-05-14 21:09:17 +0930

    Super awesome website

---
+++ public/js/script.js
@@ -0,0 +1,44 @@
+var privacy = document.getElementById("privacy");
+
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1.0,
+};
+
+const observer = new IntersectionObserver(doPrivacy, options);
+observer.observe(privacy);
+
+function doPrivacy(entries) {
+    if (!entries[0].isIntersecting) {
+        return;
+    }
+
+    p = 0
+    function makeid(length) {
+
+
+        var result = '';
+        var characters = 'BCDEFHIKLNPRSTUXYZabcdefghijklnopqrstuvxyz0123456789!#$%^&*()-_=+[]{}|;:,.<>?/';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            result += '<span>' + characters.charAt(Math.floor(Math.random() * charactersLength)) + '</span>';
+        }
+
+        if (p < 25) {
+            p++
+            privacy.innerHTML = result;
+        } else {
+            privacy.innerText = "privacy";
+            clearInterval(privacyanim);
+        }
+    }
+    var privacyanim = setInterval(makeid, 100, 5);
+}
+
+fetch("status")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("bytecounter").innerText = filesize(data.size.total_saved);
+    })
+    .catch(error => console.error('Error fetching byte counter:', error));
